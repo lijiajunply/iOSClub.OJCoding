2f98224d9bdc3516042795d807e6e7bceef6b599
李嘉俊 <2383690354@qq.com>
2024-01-12 22:29:33 +0800

    先这样算了

--- OJBlazor.Share/DataModels/TestModel.cs
+++ OJBlazor.Share/DataModels/TestModel.cs
@@ -9,10 +9,12 @@ public class TestModel
     [Key]
     [Column(TypeName = "varchar(256)")]
     public string HashName { get; set; }
+
     public string Name { get; set; }
     public string Intro { get; set; }
     public string Arg { get; set; }
     public string Code { get; set; }
+
     public override string ToString()
     {
         return $"Name={Name};Intro={Intro};Code={Code};Arg={Arg}";
@@ -23,11 +25,21 @@ public class TestModel
 /// Type:Name:Value
 /// </summary>
 /// <param name="Arg">Arg代码</param>
-public record ArgRecord(string Arg)
+public class ArgRecord
 {
-    public string Type => Arg.Split(":")[0];
-    public string Value => Arg.Split(":")[2];
-    public string Name => Arg.Split(":")[1];
+    public string Type { get; set; } = "";
+    public string Value { get; set; } = "";
+    public string Name { get; set; } = "";
+
+    public ArgRecord(string Arg)
+    {
+        Type = Arg.Split(":")[0];
+        Value = Arg.Split(":")[2];
+        Name = Arg.Split(":")[1];
+    }
+    
+    public ArgRecord(){}
 
     public static ArgRecord[] GetArray(string arg) => arg.Split(";").Select(x => new ArgRecord(x)).ToArray();
+    public override string ToString() => $"类型:{Type} 变量名称:{Name} 变量值:{Value}";
 }
